cd4cd06d96111d4b5c67193c8764f106813f35f3
vsobolyev <s_vicuk@mail.ru>
2017-08-28 05:11:56 +0200

    minor change to avoid counter overflow and attiny hang problem

--- BUTTON/ATTINY85/senderACKSleep/senderACKSleep.ino
+++ BUTTON/ATTINY85/senderACKSleep/senderACKSleep.ino
@@ -7,23 +7,27 @@
 #include <avr/sleep.h>
 #include <avr/interrupt.h>
 
+//these pins are never used.
+//configuration necessary to activate 3 pin configuration.
 #define CE_PIN 3
 #define CSN_PIN 3 //IF we are using 3 pin configuration we will use same pin for both CE and CSN, E.G. pin 3
 
 #define BTN_PIN 3 // if used a differen button to pb3, sleep interrupt button needs to be changed
-#define NRF_VCC_PIN 4
-//int NRF_VCC_PIN = 4;
+#define NRF_VCC_PIN 4 //pin that powers nrf24l01.
 
 RF24 radio(CE_PIN, CSN_PIN);  //NRF24 object
 const byte rxAddr[][7] = {"server","button"}; //device roles
 byte counter = 1; //value that will be sent to the other device
-boolean bReleased = HIGH;
+boolean bReleased = HIGH; //variable used to avoid click release being activated. in other words, when button is pressed, the action is  performed once.
 
 void setup() {
   pinMode(BTN_PIN, INPUT);
   pinMode(NRF_VCC_PIN, OUTPUT);
 }
 
+/**
+ * turn on and initialize nrf module
+ */
 void nrfON(){
 //---------------------NRF-CONFIGURATION------------------------------------------------ 
   digitalWrite(NRF_VCC_PIN,HIGH); // turn nrf_vcc ON
@@ -39,38 +43,58 @@ void nrfON(){
 //--------------------------------------------------------------------------------------  
 }
 
+/**
+ * power down first to reduce the power spike when the power is cut.
+ */
 void nrfOFF(){
   radio.powerDown(); 
   digitalWrite(NRF_VCC_PIN,LOW);//turn nrf_vcc off
 }
 
+/**
+ * send message until ack is received.
+ */
 void writeMSG(){ 
     nrfON(); //turn nrf on to send the message
     
     byte gotByte;  
-    int cont = 0;
     
     while(!radio.write( &counter, 1 )){
         delay(250);
     }
-
-    if(!radio.available()){ 
+    
+    if(!radio.available()){
+        //after the message was sent, if no ack recieved or the ack message is wrong, keep sending message.
         writeMSG(); 
     }else{
         while(radio.available() ){
+        //after tje ,essage was sent, if radio available, read check the ack message
           radio.read( &gotByte, 1 );
-                    
+        //verify ack message
           if(counter+1 != gotByte){
+            //ack message is incorrect, send message.
             writeMSG();
           }else{
-            counter++;
+            //ack was correct, increase counter.
+            if(counter > 200){
+              //byte max value is 255, to avoid overflow or malfunctionning, reset counter.
+              counter = 1;
+            }else
+              //increase counter.
+              counter++;
           }
+        }
       }
     }
 
     nrfOFF();//message recieved, turn nrf off
 }
 
+/**
+ * attiny necesary register configuration to sleep until an interrupt on pin 3 occurs.
+ * Original code taken from here:
+ * https://bigdanzblog.wordpress.com/2014/08/10/attiny85-wake-from-sleep-on-pin-state-change-code-example/
+ */
 void sleep() {
     GIMSK |= _BV(PCIE);                     // Enable Pin Change Interrupts
     PCMSK |= _BV(PCINT3);                   // Use PB3 as interrupt pin
